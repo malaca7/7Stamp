0ba4e18813d14f582b2a6933485ba8cf92d795af
malaca7 <rogeriomalaquiasjr@gmail.com>
2026-06-07 05:15:02 -0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/7Stamp/sw.js', { scope: '/7Stamp/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
